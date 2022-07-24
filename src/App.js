6620a8ebc3bef8b4d2c5e063f6f7b4f9f3bd9c67
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-07-24 10:34:35 +1000

    Github link added

--- src/App.js
+++ src/App.js
@@ -1,15 +1,22 @@
-
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-
-     
-        <p>testing testing</p>
-        <h2>please work</h2>
-
+      <p>testing testing</p>
+      <h2>please work</h2>
+      <footer>
+        {" "}
+        This project open sourced on{" "}
+        <a
+          href="https://github.com/strigenzc/weather-react"
+          target="_blank"
+          rel="noreferrer"
+        >
+          {" "}
+          Github
+        </a>
+      </footer>
     </div>
   );
 }
-
