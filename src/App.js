8ab399fe5b2dab804362b3acfc53c0507857eddb
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-07-24 11:02:51 +1000

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,30 +3,32 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <p>testing testing</p>
-      <h2>please work</h2>
-      <footer>
-        {" "}
-        This project open sourced on{" "}
-        <a
-          href="https://github.com/strigenzc/weather-react"
-          target="_blank"
-          rel="noreferrer"
-        >
+      <div className="container">
+        <p>testing testing</p>
+        <h2>please work</h2>
+        <footer>
           {" "}
-          Github
-        </a>{" "}
-        <br></br>
-        Portfolio found
-        <a
-          href="https://strigenz-portfolio.netlify.app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          {" "}
-          here
-        </a>
-      </footer>
+          This project open sourced on{" "}
+          <a
+            href="https://github.com/strigenzc/weather-react"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            Github
+          </a>{" "}
+          <br></br>
+          Portfolio found
+          <a
+            href="https://strigenz-portfolio.netlify.app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            {" "}
+            here
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
