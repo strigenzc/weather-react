15fe2636dd6663fcf806ad93157bb0d08a58ac85
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-07-24 10:30:09 +1000

    Cleaned up Code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
-import logo from "./logo.svg";
+
 import "./App.css";
 
 export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+
+     
         <p>testing testing</p>
         <h2>please work</h2>
-      </header>
+
     </div>
   );
 }
