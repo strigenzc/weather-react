d7d16197823def6f7d0f41b253504802a229e2be
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-07-29 07:46:40 +1000

    Deleted C to F conversion
    To make Forecast easier

--- src/WeatherTemperature.js
+++ src/WeatherTemperature.js
@@ -1,44 +1,10 @@
 import React, { useState } from "react";
-import "./Weather.css";
 
 export default function WeatherTemperature(props) {
-  const [unit, setUnit] = useState("metrics");
-
-  function convertToFahrenheit(event) {
-    event.preventDefault();
-    setUnit("fahrenheit");
-  }
-
-  function convertToCelsius(event) {
-    event.preventDefault();
-    setUnit("celsius");
-  }
-
-  if (unit === "celsius") {
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(props.celsius)}</span>
-        <span className="unit">
-          °C |{" "}
-          <a href="/" onClick={convertToFahrenheit}>
-            °F
-          </a>
-        </span>
-      </div>
-    );
-  } else {
-    let fahrenheit = (props.celsius * 9) / 5 + 32;
-    return (
-      <div className="WeatherTemperature">
-        <span className="temperature">{Math.round(fahrenheit)}</span>
-        <span className="unit">
-          <a href="/" onClick={convertToCelsius}>
-            °C{" "}
-          </a>{" "}
-          | °F
-        </span>
-      </div>
-    );
-  }
+  return (
+    <div className="WeatherTemperature">
+      <span className="temperature">{Math.round(props.celsius)}</span>
+      <span className="unit">°C</span>
+    </div>
+  );
 }
-//convert temp to celsius
