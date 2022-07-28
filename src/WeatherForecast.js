391290d7ad3682e3e19199924d9fae36e022261a
strigenzc <97646601+strigenzc@users.noreply.github.com>
2022-07-29 07:40:47 +1000

    Fixed Bug w/Forecast Update
    using useEffect

--- src/WeatherForecast.js
+++ src/WeatherForecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./WeatherForecast.css";
 import axios from "axios";
 import WeatherForecastDay from "./WeatherForecastDay";
@@ -6,6 +6,13 @@ import WeatherForecastDay from "./WeatherForecastDay";
 export default function WeatherForecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
+
+  //if the coordiantes change, set loaded to false
+  //aka changing the 5 day forecast to match the city change
 
   function handleResponse(response) {
     setForecast(response.data.daily);
